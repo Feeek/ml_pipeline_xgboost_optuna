f6d1ac86d44bbf75f555ffe651c06ab234d8cddb
filip <filipwielgosz2@gmail.com>
2025-05-15 10:36:12 +0200

    Zmienione komentarze

--- eda_ds_salaries_script.py
+++ eda_ds_salaries_script.py
@@ -17,18 +17,18 @@ df = df.iloc[:, 1:]
 print("\nBraki danych:")
 print(df.isnull().sum())
 
-# === 3. Statystyki opisowe ===
+# 3. Statystyki opisowe
 print("\nStatystyki opisowe:")
 print(df.describe())
 
-# === 4. Korelacje (tylko zmienne liczbowe) ===
+# 4. Korelacje (tylko zmienne liczbowe)
 plt.figure(figsize=(10, 6))
 sns.heatmap(df.corr(numeric_only=True), annot=True, cmap="coolwarm", fmt=".2f")
 plt.title("Macierz korelacji")
 plt.tight_layout()
 plt.show()
 
-# === 5. Rozkład wynagrodzenia ===
+# 5. Rozkład wynagrodzenia
 g = sns.FacetGrid(df, col="work_year", col_wrap=3, height=4, sharex=True, sharey=True)
 g.map(sns.histplot, "salary_in_usd", kde=True, bins=30)
 g.figure.suptitle("Rozkład pensji w USD na przestrzeni lat", fontsize=16)
@@ -36,7 +36,7 @@ g.tight_layout()
 g.figure.subplots_adjust(top=0.9)  # żeby tytuł się nie nakładał
 plt.show()
 
-# === 6. Box ploty wynagrodzeń ===
+# 6. Box ploty wynagrodzeń
 plt.figure(figsize=(10, 6))
 ax = sns.boxplot(
     x="work_year",
